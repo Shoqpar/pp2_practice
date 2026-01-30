a93f7994d067552d071f13c97f46481546cb36f9
Shoqpar <wigairtinst@gmail.com>
2026-01-30 22:40:15 +0500

    data types

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,17 @@
+#Numeric
+a = 5
+print (type(a)) #int
+b = 3.14
+print (type(b)) #float
+c = 1j 
+print (type(c)) #complex
+#Text
+d = "string" 
+print (type(d)) #str
+#Set
+e = {"apple", "banana", "cherry"}
+print (type(e)) #set
+#Boolean
+f = True
+print (type(f)) #bool
+#etc.
