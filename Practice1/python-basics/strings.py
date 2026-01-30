66026aed816c5665375287bedf7aeb3b6efe469b
Shoqpar <wigairtinst@gmail.com>
2026-01-30 22:49:29 +0500

    strings

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,16 @@
+print ("Hi")
+#Quotes inside quotes
+print ("My name is 'Nurali'")
+#Assigning
+a = "example"
+print (a)
+#Strings are arrays
+b = "some text"
+print (b[0])
+#Looping
+for i in b:
+    print(i)
+#Length
+print(len(b))
+#Checking the string
+print("text" in b)
